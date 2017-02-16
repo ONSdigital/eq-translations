f70f61a5b99e909fe481a865c708e34e3c18825b
Ben Jefferies <benjefferies@echosoft.uk>
2017-02-16 10:51:44 +0000

    Merge install/uninstall scripts

--- scripts/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Try to deactivate python env but don't complain if not in virtual env
-deactivate > /dev/null 2>&1 || true
-source "$(which virtualenvwrapper.sh)"
-rmvirtualenv eq-translations
