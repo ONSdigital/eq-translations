8dc92f979125f8f5de3fe996aaf4ce151b728809
Ben Jefferies <benjefferies@echosoft.uk>
2017-02-16 10:35:26 +0000

    Deactivate virtualenv

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Try to deactivate python env but don't complain if not in virtual env
+deactivate > /dev/null 2>&1 || true
 source "$(which virtualenvwrapper.sh)"
 rmvirtualenv eq-translations
