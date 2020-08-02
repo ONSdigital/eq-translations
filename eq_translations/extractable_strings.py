3a475afe9cbfa363afadb8ab8f71916777a778b8
LJBabbage <lewis.babbage@ons.gov.uk>
2020-08-02 19:05:51 +0100

    Move submission out of hub and add language_hub

--- eq_translations/extractable_strings.py
+++ eq_translations/extractable_strings.py
@@ -2,13 +2,11 @@ EXTRACTABLE_STRINGS = [
     {"json_path": "$.title", "description": "Questionnaire title"},
     {"json_path": "$.legal_basis", "description": "Questionnaire legal basis"},
     {"json_path": "$.messages.*", "description": "Global answer error message"},
-    {"json_path": "$.hub.submission.button", "description": "Hub submission button"},
-    {
-        "json_path": "$.hub.submission.guidance",
-        "description": "Hub submission guidance",
-    },
-    {"json_path": "$.hub.submission.title", "description": "Hub submission title"},
-    {"json_path": "$.hub.submission.warning", "description": "Hub submission warning"},
+    {"json_path": "$.description", "description": "Questionnaire description"},
+    {"json_path": "$.submission.button", "description": "Submission button"},
+    {"json_path": "$.submission.guidance", "description": "Submission guidance",},
+    {"json_path": "$.submission.title", "description": "Submission title"},
+    {"json_path": "$.submission.warning", "description": "Submission warning"},
     {"json_path": "$.sections[*].title", "description": "Section title"},
     {
         "json_path": "$.sections[*].repeat.title",
